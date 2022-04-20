ce978df511e6540f5eedf50b2249c7adf335aa67
Amit Dnyandeo Haral <amitharal04@gmail.com>
2022-04-20 17:44:13 +0530

    UC1...Create a LinkedList and add element to it

---
+++ LinkedListB.java
@@ -0,0 +1,21 @@
+package com.bridgelab.Day14;
+
+import java.util.LinkedList;
+
+public class LinkedListB {
+	
+	public static LinkedList<Integer> list = new LinkedList<Integer>();
+	
+	public static void UC1() {
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		
+		System.out.println(list);
+	}
+
+	public static void main(String[] args) {
+		UC1();
+	}
+
+}
